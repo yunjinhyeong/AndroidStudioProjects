4855b15b6c0505489ad3ece7969753ea9a5be4a5
yunjinhyeong <67212570+yunjinhyeong@users.noreply.github.com>
2020-10-20 16:32:16 +0900

    add finish study

--- Ex05/app/src/main/java/com/example/ex05/MainActivity.java
+++ Ex05/app/src/main/java/com/example/ex05/MainActivity.java
@@ -2,13 +2,59 @@ package com.example.ex05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+//        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+//                LinearLayout.LayoutParams.MATCH_PARENT,
+//                LinearLayout.LayoutParams.MATCH_PARENT);
+//
+//        LinearLayout baseLayout = new LinearLayout(this);
+//        baseLayout.setOrientation(LinearLayout.VERTICAL);
+//        baseLayout.setBackgroundColor(Color.rgb(0, 255, 0));
+//        setContentView(baseLayout,params);
+//
+//        Button btn = new Button(this);
+//        btn.setText("버튼입니다.");
+//        btn.setBackgroundColor(Color.MAGENTA);
+//        baseLayout.addView(btn);
+//
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(getApplicationContext(),"코드로 생성한 버튼입니다.",Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+        setTitle("IT Cookbook. Android");
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+
+        LinearLayout baseLayout = new LinearLayout(this);
+        baseLayout.setOrientation(LinearLayout.VERTICAL);
+        setContentView(baseLayout,params);
+
+        Button btn = new Button(this);
+        btn.setText("버튼입니다.");
+        btn.setBackgroundColor(Color.YELLOW);
+        baseLayout.addView(btn);
+
+        TextView tv = new TextView(this);
+        tv.setText("IT Cookbook. Android");
+        tv.setTextColor(Color.GREEN);
+        tv.setTextSize(20);
+        baseLayout.addView(tv);
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Android study in BusanIT
-#### Android layout list
+#### 1. Android Studio How to Color
+![changeColor1](imgs/changeColor1.png)
+![changeColor2](imgs/changeColor2.png)
+#### 2. Android layout list
 ![layoutList](imgs/layout.png)
